8530b630f81e53f018b51830a85c52f9504167f3
Paintface07 <paintface07@gmail.com>
2015-08-07 23:13:42 -0500

    add local node server

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,9 +1,28 @@
 'use strict';
 angular.module("angularApp", [])
-  .controller('rootController', ['$scope', '$http', function($scope, $http) {
+  .config(['$httpProvider', function($httpProvider) {
+
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $httpProvider.defaults.headers.common['Content-Type'] = "application/json";
+
+  }]).controller('rootController', ['$scope', '$http', function($scope, $http) {
 
     $scope.getStuff = function() {
-      $http.get('https://public-crest.eveonline.com/tournaments/')
+      $http.get('https://public-crest.eveonline.com/tournaments/').then(
+        function(response) {
+          $scope.myVal = response.data.items[0].href.name;
+        }, function(error) {
+          console.log("There was an error.");
+        }
+      );
     };
-    
+
+    $scope.getFromServer = function() {
+      $http.get('http://localhost:8080').then(function(response) {
+        $scope.myVal = response.data;
+      }, function(error) {
+        console.log("ERROR!");
+      });
+    };
+
   }]);
